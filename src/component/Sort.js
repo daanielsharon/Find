ba7a66ca09b816846b86c3a69f3823c9f417ccf3
Daniel Sharon <daanielsharon@gmail.com>
2022-09-03 12:57:55 +0700

    minor change

--- src/component/Sort.js
+++ src/component/Sort.js
@@ -21,11 +21,11 @@ const Sort = ({ text, selected, modifyDataSort, firstValue, secondValue, firstOp
 
 	return (
 		<div className="w-full desktop:border-solid desktop:border-b-2 desktop:border-indigo-600 tablet:border-solid tablet:border-b-2 tablet:border-indigo-600 desktop:ml-5 tablet:ml-5 mobile:ml-0 mobile:flex mobile:flex-row mobile:items-center mobile:justify-between">
-			{text}
+			<div>{text}</div>
 			<select
 				value={select}
 				label="Select Version"
-				className="text-center bg-white desktop:p-[10px] desktop:border-none desktop:ml-0 desktop:w-auto desktop:mt-0 desktop:pb-3 tablet:mt-0 tablet:w-auto tablet:ml-0 tablet:border-none tablet:p-[10px] mobile:pt-4 mobile:pb-2  mobile:border-solid mobile:border-b-2 mobile:border-indigo-600 mobile:w-4/12 "
+				className="text-center bg-white desktop:p-[10px] desktop:border-none desktop:ml-0 desktop:w-auto desktop:mt-0 desktop:pb-3 tablet:mt-0 tablet:w-auto tablet:ml-0 tablet:border-none tablet:p-[10px] mobile:p-[10px] mobile:border-solid mobile:border-b-2 mobile:border-indigo-600 mobile:w-4/12 "
 				onChange={onSelect}
 			>
 				<option value="">Unsorted</option>
